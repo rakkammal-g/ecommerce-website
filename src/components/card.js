588a8a0988d1efd998dcacfb38812568e3657c50
Rakkammal Gurusamy <rakkammal.g@ameexusa.net>
2021-03-26 09:51:37 -0700

    Added card component

---
+++ src/components/card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Card from 'react-bootstrap';
+
+class ProductCard extends React.Component {
+    render() {
+      return (
+        <Card id={this.props.product.image} style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={this.props.product.image} />
+        <Card.Body>
+          <Card.Title>{this.props.product.title}</Card.Title>
+          <Button variant="primary">Add To Cart</Button>
+        </Card.Body>
+      </Card>
+      )
+    }
+}
+
+export default ProductCard;
---
+++ src/components/collection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProductCard from './card';
+
+class ProductList extends React.Component {
+    render() {
+      return (
+       <ProductCard/>
+      )
+    }
+}
+
+export default ProductList;
